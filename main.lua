72e8730f5206673c16830c915b34992932f74a8f
mathoshi <47573361+mathoshi@users.noreply.github.com>
2019-03-01 13:30:22 -0500

    Update main.lua
    Added title

--- main.lua
+++ main.lua
@@ -15,3 +15,16 @@ BackgroundImage.x = -100
 BackgroundImage.y = 200
 BackgroundImage.id = "Background"
 
+local TitleText =
+{
+	text = "Circle Calculator 1000",
+	x = 160,
+	y = 50,
+	width = 250,
+	font = native.systemFont, 
+	fontSize = 30,
+	align = "center"
+}
+
+local Title = display.newText( TitleText )
+Title: setFillColor( 1, 1, 1 )
