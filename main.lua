087b7db994df34e1478a68510a265174e1dd5a47
mathoshi <47573361+mathoshi@users.noreply.github.com>
2019-03-01 13:07:06 -0500

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,17 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-- Created by: Matsuru Hoshi
+-- Created on Feb 29 2019
+--
+-- This file will calculate the area and circumference of a circle with a given diameter
+-----------------------------------------------------------------------------------------
+
+-- 
+
+local BackgroundImage = display.newImageRect( "assets/circle-background.png", 1080, 675 )
+BackgroundImage.x = -100
+BackgroundImage.y = 200
+BackgroundImage.id = "Background"
+
