1351938034262d5ff5772cdb75e8aacf3c353c3b
mathoshi <47573361+mathoshi@users.noreply.github.com>
2019-03-06 20:54:03 -0500

    Update CircleCalculator1000.py
    Added creation date which I added earlier but didn't save to github(2). Removed auto-destruct message. It was funny though.

--- CircleCalculator1000.py
+++ CircleCalculator1000.py
@@ -1,4 +1,5 @@
-#created by Matsuru Hoshi
+#Created by Matsuru Hoshi
+#Created on March 6, 2019
 #This file will print , "Hello, World!", to the CMD
 
 welcome = input("Welcome to the Circle Calculator 1000. Respond Ok if Ok: ")
@@ -7,13 +8,8 @@ diameter = int(input("What is the diameter? "))
 import math
 
 circumference = 2 * (math.pi * (diameter/2))
-area = math.pi * (diameter/2) ** 2 
+area = math.pi * (diameter/2) ** 2
 print( "The circumference is " + str(circumference))
 print( "The area is " + str(area))
 
 end = input( "Thank you!")
-end2 = input( "We have all your data now. Have a good day! This message will autodestruct in 3 seconds.")
-end3 = input( "3")
-end4 = input( "2")
-end5 = input( "1")
-end6 = print( "Boom!")
